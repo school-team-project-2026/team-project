98901035e44116fb8ee2cef747b634d988a98afc
mokki2975 <mokking20040701@gmail.com>
2026-09-04 14:25:22 +0900

    Feature/interest item e2e (#3)
    * chore: add postgres host to env example
    
    * feat: add frontend backend E2E connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.interest_items import router as interest_items_router
 
 app = FastAPI(title="Team Project API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(interest_items_router)
 
